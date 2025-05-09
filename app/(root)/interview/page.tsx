30a8866f9134c26dd202f4ac5cafd22bfdd5d116
aj-et <tumbokon.aaronjulius@outlook.com>
2025-05-09 13:53:54 -0600

    added interview page

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,14 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+const InterviewPage = () => {
+  return (
+    <>
+      <h3>Interview Generation</h3>
+
+      <Agent userName="John Doe" userId="123" type="interview"  />
+    </>
+  )
+}
+
+export default InterviewPage
