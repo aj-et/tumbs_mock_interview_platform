acad9ac7a092aff98dea703463821017dfc0a968
aj-et <tumbokon.aaronjulius@outlook.com>
2025-05-08 21:59:39 -0600

    added content on the homepage

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react'
 
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className='auth-layout'>{children}</div>
   )
